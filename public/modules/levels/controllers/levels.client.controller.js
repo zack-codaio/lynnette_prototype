6cb3b926bb68a6e0e026a23189e5716ce2189f80
Zachary Aman <zackyuaman@gmail.com>
2014-12-06 00:34:54 -0500

    back to selection logic

--- public/modules/levels/controllers/levels.client.controller.js
+++ public/modules/levels/controllers/levels.client.controller.js
@@ -52,12 +52,12 @@ angular.module('levels').controller('LevelsController', ['$scope', '$stateParams
             //parse kcomponents into an array
 
             //set $scope.level.kcomponenets equal to the array
-            if($scope.level.kcomponentList.length > 3){
-            var res = $scope.level.kcomponentList.split(" ");
-            //console.log(res);
+            if ($scope.level.kcomponentList.length > 3) {
+                var res = $scope.level.kcomponentList.split(" ");
+                //console.log(res);
 
-            var level = $scope.level;
-            level.kcomponents = res;
+                var level = $scope.level;
+                level.kcomponents = res;
                 delete level.kcomponentList;
             }
 
@@ -71,69 +71,69 @@ angular.module('levels').controller('LevelsController', ['$scope', '$stateParams
         // Find a list of Levels
         $scope.find = function () {
             $scope.levels = Levels.query();
-            $scope.levels.$promise.then(function(result){
+            $scope.levels.$promise.then(function (result) {
 
-            //look for mismatch of mastered and kcs
-            console.log($scope.levels);
-            for(var i = 0; i < $scope.levels.length; i++){
-                //check level mastered
+                //look for mismatch of mastered and kcs
+                console.log($scope.levels);
+                for (var i = 0; i < $scope.levels.length; i++) {
+                    //check level mastered
 
-                var curlevel = $scope.levels[i];
-                //console.log("scope mastered " + curlevel.mastered);
-                var levelmastered = result[i].mastered;
-                //console.log("levelmastered "+i+" "+levelmastered);
+                    var curlevel = $scope.levels[i];
+                    //console.log("scope mastered " + curlevel.mastered);
+                    var levelmastered = result[i].mastered;
+                    //console.log("levelmastered "+i+" "+levelmastered);
 
-                //check kc mastered for each kc
-                var kcmastered = true;
-                $scope.levels[i].progress = 100;
-                for(var j = 0; j < $scope.levels[i].kcomponents.length; j++){
-                    if($scope.levels[i].kcomponents[j].mastered == false){
-                        kcmastered = false;
+                    //check kc mastered for each kc
+                    var kcmastered = true;
+                    $scope.levels[i].progress = 100;
+                    for (var j = 0; j < $scope.levels[i].kcomponents.length; j++) {
+                        if ($scope.levels[i].kcomponents[j].mastered == false) {
+                            kcmastered = false;
+                        }
+                        if ($scope.levels[i].progress > $scope.levels[i].kcomponents[j].percentComplete) {
+                            $scope.levels[i].progress = $scope.levels[i].kcomponents[j].percentComplete;
+                        }
                     }
-                    if($scope.levels[i].progress > $scope.levels[i].kcomponents[j].percentComplete){
-                        $scope.levels[i].progress = $scope.levels[i].kcomponents[j].percentComplete;
+                    //if all kc's mastered and level mastered is false
+
+                    if (levelmastered == false && kcmastered == true) {
+                        //update level mastered
+                        $scope.levels[i].mastered = true;
+                        //update to DB
+                        for (var k = 0; k < $scope.levels[i].kcomponents.length; k++) {
+                            $scope.levels[i].kcomponents[k] = $scope.levels[i].kcomponents[k]._id;
+                        }
+                        $scope.levels[i].user = $scope.levels[i].user._id;
+
+                        $scope.levels[i]
+                            .$update(function () {
+                                $scope.find();
+                            }, function (errorResponse) {
+                                $scope.error = errorResponse.data.message;
+                                console.log($scope.error);
+                            });
+                        //trigger level complete popup
+                        $rootScope.$broadcast("levelmastered", {levelid: i});
+                        console.log("levelmastered " + i);
                     }
+                    else if (kcmastered == false && levelmastered == true) {
+                        $scope.levels[i].mastered = false;
+
+                        //update to DB
+                        for (var k = 0; k < $scope.levels[i].kcomponents.length; k++) {
+                            $scope.levels[i].kcomponents[k] = $scope.levels[i].kcomponents[k]._id;
+                        }
+                        $scope.levels[i].user = $scope.levels[i].user._id;
+
+                        $scope.levels[i]
+                            .$update(function () {
+                                $scope.find();
+                            }, function (errorResponse) {
+                                $scope.error = errorResponse.data.message;
+                            });
+                    }
+
                 }
-                //if all kc's mastered and level mastered is false
-
-                if(levelmastered == false && kcmastered == true){
-                    //update level mastered
-                    $scope.levels[i].mastered = true;
-                    //update to DB
-                    for(var k = 0; k < $scope.levels[i].kcomponents.length; k++){
-                        $scope.levels[i].kcomponents[k] = $scope.levels[i].kcomponents[k]._id;
-                    }
-                    $scope.levels[i].user = $scope.levels[i].user._id;
-
-                    $scope.levels[i]
-                        .$update(function () {
-                            $scope.find();
-                        }, function (errorResponse) {
-                            $scope.error = errorResponse.data.message;
-                            console.log($scope.error);
-                        });
-                    //trigger level complete popup
-                    $rootScope.$broadcast("levelmastered", {levelid: i});
-                    console.log("levelmastered " + i);
-                }
-                else if(kcmastered == false && levelmastered == true){
-                    $scope.levels[i].mastered = false;
-
-                    //update to DB
-                    for(var k = 0; k < $scope.levels[i].kcomponents.length; k++){
-                        $scope.levels[i].kcomponents[k] = $scope.levels[i].kcomponents[k]._id;
-                    }
-                    $scope.levels[i].user = $scope.levels[i].user._id;
-
-                    $scope.levels[i]
-                        .$update(function () {
-                            $scope.find();
-                        }, function (errorResponse) {
-                            $scope.error = errorResponse.data.message;
-                        });
-                }
-
-            }
             });
         };
 
@@ -141,13 +141,13 @@ angular.module('levels').controller('LevelsController', ['$scope', '$stateParams
         $scope.findOne = function () {
             $scope.level = Levels.get({
                 levelId: $stateParams.levelId
-            }, function(){
+            }, function () {
                 $scope.level.kcomponentList = "";
                 console.log($scope.level);
-                for(var i = 0; i < $scope.level.kcomponents.length; i++){
+                for (var i = 0; i < $scope.level.kcomponents.length; i++) {
                     $scope.level.kcomponentList = $scope.level.kcomponentList + $scope.level.kcomponents[i] + " ";
                 }
-                $scope.level.kcomponentList = $scope.level.kcomponentList.substr(0, $scope.level.kcomponentList.length-1);
+                $scope.level.kcomponentList = $scope.level.kcomponentList.substr(0, $scope.level.kcomponentList.length - 1);
             });
         };
 
@@ -155,18 +155,18 @@ angular.module('levels').controller('LevelsController', ['$scope', '$stateParams
         $scope.findOneID = function (levelId) {
             $scope.level = Levels.get({
                 levelId: levelId
-            }, function(){
+            }, function () {
                 $scope.level.kcomponentList = "";
                 console.log($scope.level);
                 $scope.level.progress = 100;
-                for(var i = 0; i < $scope.level.kcomponents.length; i++){
+                for (var i = 0; i < $scope.level.kcomponents.length; i++) {
                     $scope.level.kcomponentList = $scope.level.kcomponentList + $scope.level.kcomponents[i] + " ";
-                    if($scope.level.progress > $scope.level.kcomponents[i].percentComplete){
+                    if ($scope.level.progress > $scope.level.kcomponents[i].percentComplete) {
                         $scope.level.progress = $scope.level.kcomponents[i].percentComplete;
                     }
 
                 }
-                $scope.level.kcomponentList = $scope.level.kcomponentList.substr(0, $scope.level.kcomponentList.length-1);
+                $scope.level.kcomponentList = $scope.level.kcomponentList.substr(0, $scope.level.kcomponentList.length - 1);
 
             });
         };
@@ -202,14 +202,18 @@ angular.module('levels').controller('LevelsController', ['$scope', '$stateParams
             console.log("bad selection");
             $scope.bad_selection = true;
         }
-        $scope.$on('alreadymastered', function(){
+        $scope.$on('alreadymastered', function () {
             console.log("bad selection");
             $scope.bad_selection = true;
         });
 
-        $scope.bad_selection_next = function () {
+        $scope.bad_selection_next = function (a) {
             $scope.bad_selection = false;
+            if (a == 1) {
+                $scope.problem_screen = false;
+            }
         }
+
 
         //on good level selection
         $scope.positive_feedback_click = function () {
@@ -256,38 +260,38 @@ angular.module('levels').controller('LevelsController', ['$scope', '$stateParams
             $scope.daily_challenge_selected = $scope.daily_challenge_new;
         }
 
-        $scope.$on('KCupdated', function(){
+        $scope.$on('KCupdated', function () {
             console.log("received KCupdated");
 
             //hacky but it works?
-            setTimeout(function(){
+            setTimeout(function () {
                 $scope.find();
             }, 500);
         });
 
-        $scope.select_level = function(levelindex){
-            console.log("select_level("+levelindex+")");
+        $scope.select_level = function (levelindex) {
+            console.log("select_level(" + levelindex + ")");
             //check if mastered
             var mastered = true;
-            for(var i = 0; i < $scope.levels[levelindex].kcomponents.length; i++){
-                if($scope.levels[levelindex].kcomponents[i].mastered == false){
+            for (var i = 0; i < $scope.levels[levelindex].kcomponents.length; i++) {
+                if ($scope.levels[levelindex].kcomponents[i].mastered == false) {
                     mastered = false;
                 }
             }
-            if(mastered == true){
+            if (mastered == true) {
                 console.log('already mastered');
                 $scope.$broadcast('alreadymastered', {});
                 $rootScope.$broadcast('levelselect', {level: levelindex, mastered: true});
             }
-            else if(mastered == false){
+            else if (mastered == false) {
                 //$rootScope.$broadcast('KCbroadcast', {kcs: $scope.levels[levelindex].kcomponents});
                 $rootScope.$broadcast('levelselect', {level: levelindex, mastered: false});
             }
         }
 
-        $scope.$on('levelmastered', function(event, data){
+        $scope.$on('levelmastered', function (event, data) {
             console.log('LevelsClientController received levelmastered');
-            console.log('levelid = '+data.levelid);
+            console.log('levelid = ' + data.levelid);
 
             $scope.level_complete_message = true;
 
@@ -298,26 +302,26 @@ angular.module('levels').controller('LevelsController', ['$scope', '$stateParams
         $scope.problem_screen = false;
         $scope.hint_visible = false;
         $scope.problem = new Object();
-        $scope.$on('levelselect', function(data, args){
+        $scope.$on('levelselect', function (data, args) {
             $scope.selectedLevel = $scope.levels[args.level];
             $scope.problem_screen = true;
 
             var sequence = $scope.levels[args.level].sequence.toString();
             console.log(sequence);
 
-            var difficulty = parseInt(sequence.slice(0,1));
+            var difficulty = parseInt(sequence.slice(0, 1));
             $scope.levels[args.level].sequence = $scope.levels[args.level].sequence.slice(1) + difficulty;
             console.log($scope.levels[args.level]);
             //depopulate_level($scope.levels[args.level]).$update();
             //console.log($scope.levels[args.level].sequence);
 
 
-            var difficulty = parseInt($scope.difficulty_sequence.slice(0,1));
+            var difficulty = parseInt($scope.difficulty_sequence.slice(0, 1));
             $scope.difficulty_sequence = $scope.difficulty_sequence.slice(1) + difficulty;
-            if(difficulty > 3 || difficulty < 1){
+            if (difficulty > 3 || difficulty < 1) {
                 difficulty = 2;
             }
-            console.log("difficulty = "+difficulty);
+            console.log("difficulty = " + difficulty);
 
 
             $scope.problem.difficulty = difficulty;
@@ -325,42 +329,51 @@ angular.module('levels').controller('LevelsController', ['$scope', '$stateParams
             $scope.hint_visible = false;
         });
 
-        $scope.problem_complete = function(difficulty, levelindex){
-            console.log('problem complete');
-            $scope.problem_screen = false;
-            $rootScope.$broadcast('levelcomplete', {difficulty : difficulty, level: levelindex});
-            console.log('levelindex: '+levelindex);
-            console.log('$scope.levels: ');
-            console.log($scope.levels);
-            $rootScope.$broadcast('KCbroadcast', {kcs: $scope.levels[levelindex].kcomponents, difficulty: difficulty});
+        $scope.problem_complete = function (difficulty, levelindex, status) {
+            if (status == 1) { //proper return
+                console.log('problem complete');
+                $scope.problem_screen = false;
+                $rootScope.$broadcast('levelcomplete', {difficulty: difficulty, level: levelindex});
+                console.log('levelindex: ' + levelindex);
+                console.log('$scope.levels: ');
+                console.log($scope.levels);
+                $rootScope.$broadcast('KCbroadcast', {
+                    kcs: $scope.levels[levelindex].kcomponents,
+                    difficulty: difficulty
+                });
+            }
+            else if (status == -1) { //back to selection
+                console.log('problem complete received '+ status+' , back to selection');
+                $scope.problem_screen = false;
+            }
         }
-        $scope.hint_click = function(){
+        $scope.hint_click = function () {
             $scope.hint_visible = true;
         }
 
         $scope.aint_scared_complete = false;
-        $scope.$on('aint_scared_complete', function(event, data){
+        $scope.$on('aint_scared_complete', function (event, data) {
             console.log("received aint scared complete");
             $scope.aint_scared_complete = true;
         });
 
         $scope.hot_streak_complete = false;
-        $scope.$on('hot_streak_complete', function(event, data){
+        $scope.$on('hot_streak_complete', function (event, data) {
             console.log("received hot streak complete");
             $scope.hot_streak_complete = true;
         });
 
         $scope.trophy_case_show = false;
-        $scope.trophy_case_click = function(a){
-            if(a == 1){
+        $scope.trophy_case_click = function (a) {
+            if (a == 1) {
                 $scope.trophy_case_show = true;
             }
-            else{
+            else {
                 $scope.trophy_case_show = false;
             }
         }
     }
-]).directive('myKc', function(){
+]).directive('myKc', function () {
     console.log();
 
     //return{
